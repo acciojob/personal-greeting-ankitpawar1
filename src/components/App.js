8106b8d726bad0ca398b11835422091c2b79eb7b
ankitpawar1 <ankitpawar192837@gmail.com>
2024-09-25 10:14:36 -0700

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const nameChangeHandler = (e) => {
+    const nameValue = "Hello " + e.target.value + "!";
+    setName(nameValue);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <label htmlFor="name">Enter your name:</label>
+      <br />
+      <input onChange={nameChangeHandler} id="name" type="text" />
+      <div>{name}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
